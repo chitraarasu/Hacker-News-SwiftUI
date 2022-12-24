4f3d49d6e42c0879d6e843989186db8be359ab59
chitraarasu <kchitraarasu>
2022-12-24 18:24:26 +0530

    Initial Commit

---
+++ Hacker News SwiftUI/Hacker_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_SwiftUIApp.swift
+//  Hacker News SwiftUI
+//
+//  Created by kirshi on 12/24/22.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
